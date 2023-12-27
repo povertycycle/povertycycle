0702cf1f2ffe82ac6aa7031ac55ee309821a08cc
William <william.sentosatio@hotmail.com>
2023-12-28 03:05:15 +0700

    codex option improvements

--- src/common/components/portfolio/resume/contents/codex.tsx
+++ src/common/components/portfolio/resume/contents/codex.tsx
@@ -21,16 +21,18 @@ const JOURNALS : Journal = {
                 "Nothing is more fascinating than to fully understand the intricacies behind the work of art and music of a game or a piece of cinema.",
                 "Your journey started off with the classics\u2014Star Wars, Jurassic Park, Terminator, and many more\u2014not exactly old though gold nonetheless. Yet from the start, that enjoyment for them was however a shallow and unappreciative one, ignorant of their real beauty.",
                 "And with the years of exposure, stumbling across those of which values were burrowed deep within their concepts and designs, the curiosity for their details and techniques grew stronger. Suddenly, the aspiration bloomed, to fully understand the idea behind such celebrated works, and to be able to relish such feelings with the community.",
-                "How amazing it is that we were blessed with such magnificence, for there is a whole world of imagination to be delved into; the works of art that transcend beyond time and space\u2014those that resonate uniquely to their fans\u2014those that can be appreciated by anyone of any background. And so, it was believed that: to truly appreciate their endless creativity, is to really be a human."
+                "How amazing it is that we were blessed with such magnificence, for there is a whole world of imagination out there waiting to be explored; the works of art that transcend beyond time and space\u2014those that resonate uniquely to their fans\u2014those that can be appreciated by anyone of any background. And so, it was believed that: to truly appreciate their endless creativity, is to really be a human."
             ]
         }
     },
     1 : {
         title: "Website",
         content: {
-            heading:                 "A software engineer with a minor in mathematics.",
+            heading: "A software engineer with a minor in mathematics.",
             description: [
-                "This website was designed and developed as a love letter for their [efforts] in [guiding] everyone through the journeys they made\u2014filled with a [range of emotions]. From the rain, the bubbles, the fishes, and the buildings, all were randomly generated with quadratic equations. And all the icons and shapes were made purely from CSS; no image files were stored in repository.",
+                "The time does not matter; what matters is the completion.",
+                "This personal website was designed and developed as a love letter for the games and the cinemas. It incorporated a number of different styles and concepts to help enrich the experience it meant to provide.",
+                "The rain, bubbles, fishes, and buildings were generated randomly with quadratic equations. The icons and shapes were made purely with CSS; no image files were stored in the repository.",
             ]
         }
     },
@@ -38,8 +40,8 @@ const JOURNALS : Journal = {
         acc[index + 2] = {
             title: "??????????",
             content: {
-                heading: "",
-                description: [""],
+                heading: "??????????",
+                description: ["??????????"],
             },
         };
         return acc;
@@ -48,9 +50,9 @@ const JOURNALS : Journal = {
 
 const CodexScroller : React.FC<{ scroll: number }> = ({ scroll }) => {
     return (
-        <div className="flex flex-col items-center justify-between py-2 gap-2">
+        <div className="flex flex-col h-[80%] items-center justify-between py-2 gap-2">
             <div className={styles.codexArrowUp}></div>
-            <div className="h-[80%] w-[0.25rem] bg-default-white rounded-full flex justify-center relative">
+            <div className="h-[85%] w-[0.25rem] bg-default-white rounded-full flex justify-center relative">
                 <div className="w-4 h-4 bg-default-white rounded-full absolute mt-[-0.5rem]" style={{
                     top: `${scroll}%`
                 }}/>
@@ -75,9 +77,9 @@ const JournalOptions : React.FC<{
     };
 
     return (
-        <div className={`${styles.codexJournalContainer} h-full w-[30%] bg-black bg-opacity-50`}>
+        <div className={`${styles.codexJournalContainer} h-full w-[30%] bg-black bg-opacity-50 flex items-center`}>
             <div className={`flex flex-col gap-2 h-full w-full overflow-y-scroll relative ${styles.overflowContainer}`} onScroll={handleScroll}>
-                <div className="w-full h-full absolute">
+                <div className="w-full h-full absolute flex flex-col gap-2">
                     {
                         Object.values(JOURNALS).map((journal, index: number) => {
                             const handleChangeActive = () => {
@@ -85,7 +87,7 @@ const JournalOptions : React.FC<{
                             }
                             
                             return (
-                                <div className={`flex cursor-pointer gap-8 px-4 py-1 bg-sea-blue-dark ${active == index ? "bg-opacity-90" : "bg-opacity-40 hover:bg-opacity-65"}`} key={index} onClick={handleChangeActive}>
+                                <div className={`flex select-none cursor-pointer gap-8 px-4 py-1 bg-gradient-to-r to-transparent transition-transform ${active == index ? "border-l-4 border-white from-sea-blue-dark/100 translate-x-[2rem]" : "from-sea-blue-dark/50 hover:from-sea-blue-dark/75 hover:translate-x-[2rem]"}`} key={index} onClick={handleChangeActive}>
                                     <span>{index.toString().padStart(3, "0")}</span>
                                     <span>{journal.title}</span>    
                                 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,38 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/common/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      borderColor: {
-        'rain': '#A1C6CC',
-        'sea-green': '#1a827c',
-        'sea-green-dark': '#0d403d',
-        'sea-fluorescent': '#97fffa',
-      },
-      textColor: {
-        'neon': '#E1F5FF',
-      },
-      fontFamily: {
-        'andes': ['andes'],
-        'market-deoc': ['market-deco'],
-      },
-      backgroundColor: {
-        'default-white': '#FAFAFA',
-        'sea-blue-dark': '#136087',
-        'sea-green': '#1a827c',
-        'sea-green-dark': '#0d403d',
-      }
-    },
-  },
-  variants: {
-    extend: {
-        backgroundColor: ['active'],
-        transform: ['active', 'hover', 'focus'],
-    }
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/common/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+				'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				'rain':'#A1C6CC',
+				'neon': '#E1F5FF',
+				'sea-blue-dark': '#136087',
+				'default-white': '#FAFAFA',
+				'sea-green-dark': '#0d403d',
+				'sea-green': '#1a827c',
+			},
+			fontFamily: {
+				'andes': ['andes'],
+				'market-deoc': ['market-deco'],
+			},
+		},
+	},
+	variants: {
+		extend: {
+			backgroundColor: ['active'],
+			transform: ['active', 'hover', 'focus'],
+		}
+	},
+	plugins: [],
 }
 export default config
