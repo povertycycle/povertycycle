b8dc85b8a61c6d3ea6b12a3fe5520ca6e2b9e115
William <william.sentosatio@hotmail.com>
2024-02-09 21:22:39 +0700

    basePath

--- src/common/components/portfolio/resume/rapture.tsx
+++ src/common/components/portfolio/resume/rapture.tsx
@@ -3,7 +3,7 @@ import Bubbles from "./rapture/bubbles";
 import Buildings from "./rapture/buildings";
 import Fishes from "./rapture/fishes";
 
-const Rapture : React.FC = () => {
+const Rapture: React.FC = () => {
     return (
         <div className="w-full h-full relative z-[1]">
             <Bubbles />
